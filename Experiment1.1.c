b4df71cb521e2fee1df9b94210a9b907a29a218e
rifaahmemon25 <rifaahmemon@eng.rizvi.edu.in>
2025-10-15 00:29:41 +0530

    Create Experiment1.1.c

---
+++ Experiment1.1.c
@@ -0,0 +1,14 @@
+/*
+Name: Rifaah Memon
+UIN: 251P119
+Class: Comp Eng-D
+*/
+#include <stdio.h>
+
+int main() {
+  printf("Name: Rifaah Memon \nAge: 17\n");
+  printf("UIN: 251P119");
+  printf("Class: F.E, Div:D");
+
+return 0;
+}
