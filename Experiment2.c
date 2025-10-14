0a5436a5e79eb2d9c7fd29543e171b3641a480d7
rifaahmemon25 <rifaahmemon@eng.rizvi.edu.in>
2025-10-14 21:55:48 +0530

    Add files via upload

---
+++ Experiment2.c
@@ -0,0 +1,24 @@
+/*
+Name: Noushaba Siddiqui
+UIN:251P1080
+COMPUTER ENGINEERING D2
+*/
+#include<stdio.h>
+
+int main(){
+int lim;
+int sum=0;
+printf("enter the limit :");
+scanf("%d",&lim);
+for(int i=0;i<=lim;i++){
+
+if(i%2!=0){
+
+sum+=i;
+}
+ }
+
+printf("sum of odd is %d", sum);
+
+return 0;
+ }
