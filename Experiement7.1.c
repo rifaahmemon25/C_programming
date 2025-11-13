eebf921cba848ea6f4efdf7eacdda41c835aab9c
rifaahmemon25 <rifaahmemon@eng.rizvi.edu.in>
2025-11-13 21:51:24 +0530

    Add header comment with author and course info

---
+++ Experiement7.1.c
@@ -0,0 +1,28 @@
+/*Rifaah Memon
+251P119 41
+COMP ENG Div:D*/
+#include <stdio.h>
+
+int main() {
+    int i, n , a[10], largest;
+
+    printf("Enter how many numbers: ");
+    scanf("%d", &n);
+
+    printf("Enter the numbers:\n");
+    for(i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    largest = a[0];
+
+    for(i = 1; i < n; i++) {
+        if(a[i] > largest) {
+            largest = a[i];
+        }
+    }
+
+    printf("Largest number = %d\n", largest);
+
+    return 0;
+}
