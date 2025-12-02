3205b06e7d70845b72b59f4ef36cc5742a497474
rifaahmemon25 <rifaahmemon@eng.rizvi.edu.in>
2025-12-02 20:19:37 +0530

    Create Experiment10.2.c

---
+++ Experiment10.2.c
@@ -0,0 +1,15 @@
+/*Rifaah Memon
+Roll No 41 UIN 251P119
+COMP ENG Div D*/
+#include <stdio.h>
+
+int main() {
+    int array[]={10,20,30,40,50};
+    int n=sizeof(array)/sizeof(array[0]);
+    int*p=array;
+    for(int i= n - 1; i>=0;i--){
+        printf("%d ",*(p + i));
+    }
+
+    return 0;
+}
